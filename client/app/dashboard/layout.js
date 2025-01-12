b342ccaba67230337bc20ebc576cc8fc1451cf79
Alexandra Shbaro <alexandrashbaro@gmail.com>
2025-01-12 16:10:54 +0200

    modified layout to view header and sidebar properly with children

--- client/app/dashboard/layout.js
+++ client/app/dashboard/layout.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 
-const DashboardLayout= ({ children }) => {
-  return (
-    <div>
-      <Header/>
-      {children}
-      <Sidebar/>
-      
-    </div>
-  );
-};
 
-export default DashboardLayout;
-
+export default function DashboardLayout({ children }) {
+    return (
+      <div className="min-h-screen bg-[#FAFAFA]">
+        <Sidebar />
+        <div className="pl-64">
+          <Header />
+          <main className="p-6">{children}</main>
+        </div>
+      </div>
+    );
+  }
+  
