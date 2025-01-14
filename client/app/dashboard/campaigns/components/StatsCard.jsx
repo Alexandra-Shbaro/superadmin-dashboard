6e140bbba910dc7a0e127c2de5122cd109fb5663
Alexandra Shbaro <alexandrashbaro@gmail.com>
2025-01-14 22:58:04 +0200

    changed styling of stats cards

--- client/app/dashboard/campaigns/components/StatsCard.jsx
+++ client/app/dashboard/campaigns/components/StatsCard.jsx
@@ -1,8 +1,8 @@
 export function StatsCard({ title, value }) {
     return (
-      <div className="rounded-lg bg-mediumGrey p-4 border border-softBlack/20">
-        <p className="text-sm text-lightGrey">{title}</p>
-        <p className="mt-2 text-2xl font-bold text-offWhite">{value}</p>
+      <div className="rounded-lg bg-offWhite p-4 border border-softBlack/20">
+        <p className="text-sm text-softBlack">{title}</p>
+        <p className="mt-2 text-2xl font-bold text-gray-800">{value}</p>
       </div>
     )
   }
